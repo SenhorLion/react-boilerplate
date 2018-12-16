504f36e7fed07f5a7e00121b931a324ae4034668
Lionel Adams <adamslionel@hotmail.co.uk>
2018-12-16 18:36:12 +0000

    Upadte with webpack dev and prod configs, enable source maps in dev

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+    debugger;
+    return <h1>Hello Reaction - from webpack dev server</h1>;
+  }
+}
+
+export default App;
