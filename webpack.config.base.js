3de8b961b872bb608267b774f399ed9198d098a2
Lionel Adams <adamslionel@hotmail.co.uk>
2019-01-16 22:12:50 +0000

    Add bundle reducers based on target browsers

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -20,7 +20,20 @@ module.exports = {
         include: [path.resolve(__dirname, 'src')],
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: [
+                  'last 2 versions',
+                  'not dead',
+                  'not < 2%',
+                  'not ie 11',
+                ],
+              },
+            ],
+            '@babel/preset-react',
+          ],
           plugins: [
             'react-hot-loader/babel',
             '@babel/plugin-proposal-class-properties',
